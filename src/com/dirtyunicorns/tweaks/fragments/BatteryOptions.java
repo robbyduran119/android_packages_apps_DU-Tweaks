e4e3775c963d68bad1c048ea00d5fa25c5ce7ea5
storm119 <storm119@gmail.com>
2018-02-21 22:40:49 +0800

    Revert "Move battery style settings to dutweaks [2/2]"
    This reverts commit 25c40737ece0ff809e646a5b9eb778c99d6ab108.

--- src/com/dirtyunicorns/tweaks/fragments/BatteryOptions.java
+++ src/com/dirtyunicorns/tweaks/fragments/BatteryOptions.java
@@ -16,7 +16,6 @@
 
 package com.dirtyunicorns.tweaks.fragments;
 
-import android.content.ContentResolver;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -34,49 +33,14 @@ import com.android.internal.logging.nano.MetricsProto;
 
 public class BatteryOptions extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-    private static final String BATTERY_STYLE = "battery_style";
-    private static final String SHOW_BATTERY_PERCENT = "show_battery_percent";
-
-    private ListPreference mBatteryIconStyle;
-    private SwitchPreference mBatteryPercentage;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.battery_options);
-        final ContentResolver resolver = getActivity().getContentResolver();
-
-        int batteryStyle = Settings.Secure.getInt(resolver,
-                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
-        mBatteryIconStyle = (ListPreference) findPreference(BATTERY_STYLE);
-        mBatteryIconStyle.setValue(Integer.toString(batteryStyle));
-        mBatteryIconStyle.setOnPreferenceChangeListener(this);
-
-        boolean show = Settings.System.getInt(resolver,
-                Settings.System.SHOW_BATTERY_PERCENT, 1) == 1;
-        mBatteryPercentage = (SwitchPreference) findPreference("show_battery_percent");
-        mBatteryPercentage.setChecked(show);
-        mBatteryPercentage.setOnPreferenceChangeListener(this);
-        boolean hideForcePercentage = batteryStyle == 6 || batteryStyle == 7; /*text or hidden style*/
-        mBatteryPercentage.setEnabled(!hideForcePercentage);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mBatteryIconStyle) {
-            int value = Integer.valueOf((String) newValue);
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, value);
-            boolean hideForcePercentage = value == 6 || value == 7;/*text or hidden style*/
-            mBatteryPercentage.setEnabled(!hideForcePercentage);
-            return true;
-        } else  if (preference == mBatteryPercentage) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SHOW_BATTERY_PERCENT, value ? 1 : 0);
-            mBatteryPercentage.setChecked(value);
-            return true;
-        }
         return false;
     }
 
