0b21e981e01acae54f821de8ef55f9f7e84e9493
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-10 23:16:22 +0530

    DT: Navigation: let people disable both gestures and navbar  * @storm119 edits:  - very useful if using other gesture method such as edge or stand alone gesture app
    Change-Id: I15dcfd846af70df549849af95597599718ad5297
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/NavigationBar.java
+++ src/com/dirtyunicorns/tweaks/fragments/NavigationBar.java
@@ -166,7 +166,6 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {
         mNavbarVisibility.setChecked(showing);
-        mGestureNavigation.setChecked(!showing);
         mGestureNavigation.setEnabled(!showing);
         mNavInterface.setEnabled(mNavbarVisibility.isChecked());
         mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
@@ -174,10 +173,9 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
 
     private void updateGestures(boolean showing) {
         mGestureNavigation.setChecked(showing);
-        mNavbarVisibility.setChecked(!showing);
         mNavbarVisibility.setEnabled(!showing);
-        mNavInterface.setEnabled(mNavbarVisibility.isChecked());
-        mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
+        mNavInterface.setEnabled(mNavbarVisibility.isEnabled());
+        mNavGeneral.setEnabled(mNavbarVisibility.isEnabled());
     }
 
     @Override
@@ -196,8 +194,6 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
             boolean showing = ((Boolean)newValue);
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
                     showing ? 1 : 0);
-            Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
-                    showing ? 0 : 1);
             updateBarVisibleAndUpdatePrefs(showing);
             mHandler.postDelayed(new Runnable() {
                 @Override
@@ -210,8 +206,6 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
             boolean showing = ((Boolean)newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
                     showing ? 1 : 0);
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
-                    showing ? 0 : 1);
             updateGestures(showing);
             return true;
         } else if (preference == mBarHeightPort) {
